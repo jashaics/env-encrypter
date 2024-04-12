1d08bf28ba358543a412dc4fd66c726b2c5c0f07
Jacopo Viscuso <jacopo.viscuso@gmail.com>
2024-04-12 11:51:39 +0200

    test: Set up pest environment

---
+++ tests/PackageTestCase.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Jashaics\EnvEncrypter\Tests;
+
+use Jashaics\EnvEncrypter\EnvEncrypterServiceProvider;
+use Orchestra\Testbench\TestCase;
+
+class PackageTestCase extends TestCase
+{
+    protected function getPackageProviders($app): array
+    {
+        return [
+            EnvEncrypterServiceProvider::class,
+        ];
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,55 +1,10 @@
 <?php
 
-namespace Jashaics\EnvEncrypter\Tests;
+namespace Tests;
 
-use Jashaics\EnvEncrypter\EnvEncrypterServiceProvider;
-use Orchestra\Testbench\TestCase as Orchestra;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
-abstract class TestCase extends Orchestra
+abstract class TestCase extends BaseTestCase
 {
-    protected string $localEnvFile = '.env';
-
-    protected string $previewEnvFile = '.env.preview';
-
-    protected string $productionEnvFile = '.env.production';
-
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        // creating .env files
-        file_put_contents($this->localEnvFile, '# this is local env file');
-        file_put_contents($this->previewEnvFile, '# this is preview env file');
-        file_put_contents($this->productionEnvFile, '# this is prodduction env file');
-    }
-
-    public function tearDown(): void
-    {
-        parent::tearDown();
-
-        // cleaning up .env files
-        unset($this->localEnvFile);
-        unset($this->previewEnvFile);
-        unset($this->productionEnvFile);
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            EnvEncrypterServiceProvider::class,
-        ];
-    }
-
-    /**
-     * Given a file path, return the path to the file in the package
-     */
-    protected function getFilePathInPackage(string $filePath): string
-    {
-        return realpath(__DIR__.'/../').'/'.trim($filePath, '/');
-    }
-
-    // protected function getEnvironmentSetUp($app)
-    // {
-    //   // perform environment setup
-    // }
+    //
 }
